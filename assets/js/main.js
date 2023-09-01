6c518ddf5b5ae286b68ff821c35f25a494c7a662
EliaBuratti <elia.buratti.dev@gmail.com>
2023-09-01 16:07:58 +0200

    complete ex

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,4 +12,57 @@ Le validazioni e i controlli possiamo farli anche in un secondo momento. */
 
 
 
-console.log('ciao mondo');
+const startGame = document.querySelector('.start-game > button');
+const startMarkup = document.querySelector('section.start-game');
+const gridFieldSection = document.querySelector('section.grid-field');
+let gameMission = 'Clicca su una casella'
+
+
+//al click del pulsante
+startGame.addEventListener('click', function(){
+
+    //rimuovo markup iniziale
+    removeMarkup(startMarkup);
+
+    //genero un nuovo markup con il titolo del gioco
+    gridFieldSection.innerHTML = `<h1 class="w-100 mb-4" >${gameMission}</h1>`; // 'Clicca su una casella'
+
+    //uso una funzione per creare delle card cliccabili
+    genCardEvent(gridFieldSection, 100);
+
+
+});
+
+//funzione per rimuovere il markup interno ad una selezione
+function removeMarkup (markup) {
+
+    markup.innerHTML = '';
+};
+
+// funzione per generare le card con un eventlistener con input: dove(domElement) e quante card da creare (targhet)
+function genCardEvent (domElement, targhet) {
+
+    for (let i = 0; i < targhet; i++) {
+
+        //genero la card
+        const genElement = document.createElement('div');
+        genElement.className = 'eb_card';
+        genElement.append(i + 1);
+        domElement.append(genElement);
+
+        //genero eventlistener per ogni card creata
+        genElement.addEventListener('click', function(){
+
+            this.classList.toggle('eb_active');//attiva o disattiva classe
+
+            //stampo in console la card cliccata
+            console.log('Hai cliccato nella casella', i + 1);
+
+            //*****************temporaneo, solo per stamparlo in pagina**************************
+            document.querySelector('section.grid-field > h1').innerHTML = `Hai cliccato nella casella: ${i + 1}`; // Hai cliccato nella casella: ${i + 1}
+
+        }); 
+        
+    };
+
+};
